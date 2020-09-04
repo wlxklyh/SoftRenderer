018ab67e70ac79b654d40d931d32eee9905ab9d7
wlxklyh <linyanhou@qq.com>
2020-09-04 14:12:58 +0800

    把像素Raw data 转程UIImage显示出来 为后面CPU渲染铺垫
    把像素Raw data 转程UIImage显示出来 为后面CPU渲染铺垫

--- iOS/SoftRenderer/SoftRenderer/ViewController.m
+++ iOS/SoftRenderer/SoftRenderer/ViewController.m
@@ -11,6 +11,7 @@
 @interface ViewController (){
     
 }
+- (UIImage*)convertBufferToUIImage:(void*)rawImagePixels width:(int)width height:(int)height;
 
 @property UIImageView * ContentView;
 @end
@@ -19,17 +20,52 @@
 
 @synthesize ContentView;
 
+- (UIImage*)convertBufferToUIImage:(void*)rawImagePixels width:(int)width height:(int)height
+{
+    int totalBytesForImage = width * height * 4;
+    CGDataProviderRef dataProvider = CGDataProviderCreateWithData(NULL, rawImagePixels, totalBytesForImage, dataProviderReleaseCallback);
+    CGColorSpaceRef defaultRGBColorSpace = CGColorSpaceCreateDeviceRGB();
+    CGImageRef cgImageFromBytes = CGImageCreate(width, height, 8, 32, 4 * width, defaultRGBColorSpace, kCGBitmapByteOrderDefault | kCGImageAlphaLast, dataProvider, NULL, NO, kCGRenderingIntentDefault);
+    CGDataProviderRelease(dataProvider);
+    CGColorSpaceRelease(defaultRGBColorSpace);
+    UIImage *image = [UIImage imageWithCGImage:cgImageFromBytes];
+    return image;
+
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    NSLog(@"View Did Load");
-    self.view.backgroundColor = [UIColor greenColor];
-    // Do any additional setup after loading the view.
-    self.ContentView = [[UIImageView alloc] initWithFrame:CGRectMake(100, 100, 200, 200)];
-    self.ContentView.backgroundColor = [UIColor redColor];
-    [self.view addSubview:self.ContentView];
     
+    self.view.backgroundColor = [UIColor blueColor];
+    
+    self.ContentView = [[UIImageView alloc] initWithFrame:CGRectMake(100, 100, 200, 200)];
+    
+    char* rawImagePixels = malloc(200*200*4);
+    for(int i=0;i<200;i++)
+    {
+        for (int j=0; j<200; j++) {
+            rawImagePixels[(i*200+j)*4+0] = 0;
+            rawImagePixels[(i*200+j)*4+1] = 255;
+            rawImagePixels[(i*200+j)*4+2] = 0;
+            rawImagePixels[(i*200+j)*4+3] = 255;
+        }
+    }
+    UIImage* image = [self convertBufferToUIImage: rawImagePixels width : 200 height :200];
+    self.ContentView.image = image;
+    
+    //free(rawImagePixels);
+    [self.view addSubview:self.ContentView];
 }
+
+
+
+void dataProviderReleaseCallback (void *info, const void *data, size_t size)
+{
+    free((void *)data);
+}
+
 
 
 @end
